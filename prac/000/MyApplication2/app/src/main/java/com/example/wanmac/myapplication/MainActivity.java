c0d307283ca44143d63fedc417cff2fe3e8cf3c8
wanMac <wanaaaa@gmail.com>
2016-07-18 14:21:15 -0400

    asdf

--- prac/000/MyApplication2/app/src/main/java/com/example/wanmac/myapplication/MainActivity.java
+++ prac/000/MyApplication2/app/src/main/java/com/example/wanmac/myapplication/MainActivity.java
@@ -1,13 +1,17 @@
 package com.example.wanmac.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+    Context context;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPref = context.getSharedPreferences("com.example.myapp.SOME_KEY_NAME", Context.MODE_PRIVATE);
     }
 }
